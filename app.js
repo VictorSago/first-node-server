7525840cf67177de91b68e537f6fb0ea1099d55a
Victor Sago <victor.sago@elstandard.se>
2022-10-11 15:55:08 +0200

    Simple request-response handling

--- app.js
+++ app.js
@@ -2,8 +2,35 @@
 const http = require('http');
 
 
+/* const server = http.createServer((req, res) => {
+  //console.log(req);
+  console.log("URL: ", req.url);
+  console.log("Method: ", req.method);
+  console.log("Headers: ", req.headers);
+  res.setHeader('Constent-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>First Node Server Page</title></head>');
+  res.write('<body><h1>Node Server Active!</h1></body>')
+  res.write('</html>');
+  res.end();
+}); */
+
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  if (url === '/') {
+    res.setHeader('Constent-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+    res.write('</html>');
+    return res.end();
+  }
+  res.setHeader('Constent-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>First Node Server Page</title></head>');
+  res.write('<body><h1>Node Server Active!</h1></body>')
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
