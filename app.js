e8137b64ff13519cd40ffb3c54ddcbd30e57fba2
Victor Sago <victor.sago@elstandard.se>
2022-10-15 12:58:50 +0200

    Small corrections

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const http = require('http');
   console.log("URL: ", req.url);
   console.log("Method: ", req.method);
   console.log("Headers: ", req.headers);
-  res.setHeader('Constent-Type', 'text/html');
+  res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>First Node Server Page</title></head>');
   res.write('<body><h1>Node Server Active!</h1></body>')
@@ -18,14 +18,14 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === '/') {
-    res.setHeader('Constent-Type', 'text/html');
+    res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
     res.write('</html>');
     return res.end();
   }
-  res.setHeader('Constent-Type', 'text/html');
+  res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>First Node Server Page</title></head>');
   res.write('<body><h1>Node Server Active!</h1></body>')
